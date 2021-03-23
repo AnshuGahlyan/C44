3a754b64f1bdbba1d9e892830fe37f75bce2e552
AnshuGahlyan <71934013+AnshuGahlyan@users.noreply.github.com>
2021-03-23 20:55:06 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -222,7 +222,7 @@ if(points < -80){
     fill("green");
     text("Tank",900,500);
     textSize(20);
-    text("Press M to continue",1300,600);
+    text("Press M to continue",1320,600);
   }else{
     a.x = 2000;
     b.x = 2000;
